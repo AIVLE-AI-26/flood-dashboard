d72de757cd52f08d2e9ab9eaa068cfb42c91d87f
bootkorea <alphasobs@gmail.com>
2024-06-27 04:35:59 +0000

    initialize

--- service/core/settings.py
+++ service/core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
