63d27e6d04aa116c0385f5804360cef0c01e8905
yuri <dydrkagksekf@naver.com>
2024-07-07 19:19:33 +0900

    [디자인,기능] 메인화면 수정, 로그인, 회원가입 화면 수정, 대피소 주소명 수정, 게시판 기능 수정 - 지도 상단 버튼 수정(침수예상 지역, 대피소) - footer 제거 - 사이드바 메뉴명 통일 - 상단바 헤더 내용 변경 - 대피소 지역 드롭다운 메뉴로 변경 - 대피소 데이터 주소명 '새주소' 제거 - db연결(sqllite3 사용) - db 연결 시 작성자, 작성일, 수정시간, 내용, 파일 업로드, 조회수 - 게시판 글쓰기, 수정, 삭제 구현

--- service/ToDoList/board/urls.py
+++ service/ToDoList/board/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('write/', views.board_write, name='board_write'),
-    path('list/', views.board_list, name='board_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
