8f33b811ece9611fef4b1988ea87280a7c57a3ce
syu357 <syeonshine_27@naver.com>
2024-07-11 11:30:48 +0900

    [디자인] 하천 수위 단위 수정 및 정렬 .25 -> 0.25m, 하천 이름 오름차순 정렬

--- service/ToDoList/waterlevel/static/js/waterlevel.js
+++ service/ToDoList/waterlevel/static/js/waterlevel.js
@@ -5,21 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
     // 관측소의 위치와 수위를 표시할 데이터 (중앙으로 모이도록 설정)
     var observatories = [
         { name: '담양군(광주댐)', x: 711, y: 278, wl: '0.00' },
-        { name: '광주광역시(용산교)', x: 512, y: 171, wl: '0.00' },
-        { name: '광주광역시(첨단대교)', x: 447, y: 219, wl: '0.00' },
-        { name: '광주광역시(유촌교)', x: 447, y: 324, wl: '0.00' },
-        { name: '광주광역시(풍영정천2교)', x: 357, y: 314, wl: '0.00' },
-        { name: '광주광역시(어등대교)', x: 376, y: 338, wl: '0.00' },
-        { name: '광주광역시(신운교)', x: 517, y: 320, wl: '0.00' },
-        { name: '광주광역시(우제교)', x: 625, y: 399, wl: '0.00' },
-        { name: '광주광역시(설월교)', x: 593, y: 402, wl: '0.00' },
-        { name: '광주광역시(벽진동)', x: 422, y: 401, wl: '0.00' },
-        { name: '광주광역시(장록교)', x: 298, y: 392, wl: '0.00' },
-        { name: '광주광역시(평림교)', x: 100, y: 327, wl: '0.00' },
-        { name: '광주광역시(천교)', x: 551, y: 358, wl: '0.00' },
-        { name: '광주광역시(용진교)', x: 218, y: 224, wl: '0.00' },
-        { name: '광주광역시(승용교)', x: 271, y: 523, wl: '0.00' },
         { name: '광주광역시(극락교)', x: 382, y: 389, wl: '0.00' },
+        { name: '광주광역시(벽진동)', x: 422, y: 401, wl: '0.00' },
+        { name: '광주광역시(설월교)', x: 593, y: 402, wl: '0.00' },
+        { name: '광주광역시(승용교)', x: 271, y: 523, wl: '0.00' },
+        { name: '광주광역시(신운교)', x: 517, y: 320, wl: '0.00' },
+        { name: '광주광역시(어등대교)', x: 376, y: 338, wl: '0.00' },
+        { name: '광주광역시(용산교)', x: 512, y: 171, wl: '0.00' },
+        { name: '광주광역시(용진교)', x: 218, y: 224, wl: '0.00' },
+        { name: '광주광역시(우제교)', x: 625, y: 399, wl: '0.00' },
+        { name: '광주광역시(유촌교)', x: 447, y: 324, wl: '0.00' },
+        { name: '광주광역시(장록교)', x: 298, y: 392, wl: '0.00' },
+        { name: '광주광역시(천교)', x: 551, y: 358, wl: '0.00' },
+        { name: '광주광역시(첨단대교)', x: 447, y: 219, wl: '0.00' },
+        { name: '광주광역시(평림교)', x: 100, y: 327, wl: '0.00' },
+        { name: '광주광역시(풍영정천2교)', x: 357, y: 314, wl: '0.00' },
     ];
 
     // 전체 수위 데이터를 오른쪽 패널에 표시
@@ -62,13 +62,18 @@ document.addEventListener('DOMContentLoaded', function() {
             method: "GET",
             success: function(data) {
                 data.forEach(function(station) {
+                    var wl = station.wl.toString();
+                    if (wl.startsWith('.')) {
+                        wl = '0' + wl;
+                    }
+                    
                     var marker = document.querySelector(`.map-marker[data-name="${station.obsnm}"]`);
                     if (marker) {
-                        marker.setAttribute('data-wl', station.wl);
+                        marker.setAttribute('data-wl', wl);
                     }
                     var item = document.querySelector(`.info-item[data-name="${station.obsnm}"]`);
                     if (item) {
-                        item.innerHTML = `<b>${station.obsnm}</b><br>현재 수위: ${station.wl}m`;
+                        item.innerHTML = `<b>${station.obsnm}</b><br>현재 수위: ${wl}m`;
                     }
                 });
             }
