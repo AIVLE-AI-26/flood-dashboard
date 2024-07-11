3ad845d72c6212524a935291a446e12d75c5f9b8
pnj <PNamju@example.com>
2024-07-11 17:05:34 +0900

    [디자인] 로그인, 회원가입 디자인 수정 -로그인, 회원가입의 html,css 수정 -main/css/style.css 수정

--- service/ToDoList/login/views.py
+++ service/ToDoList/login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import LoginForm
+from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
@@ -12,9 +13,11 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect('home')  # 로그인 후 메인 페이지로 리디렉션
+        else:
+            messages.error(request, '아이디 혹은 비밀번호가 틀렸습니다.')
     else:
         form = LoginForm()
-    return render(request, 'login/login.html', {'form': form})
+    return render(request, 'login/login.html', {'form': form, 'messages' : messages.get_messages(request)})
 
 def logout_view(request):
     logout(request)
