8d39c692a92f82019a598ddfa972534e6b2a9383
yuri <dydrkagksekf@naver.com>
2024-07-12 16:42:21 +0900

    [기능] 회원가입 수정 - 관리자/ 일반 사용자 가입 수정 - 관리자는 @aivle.co.kr로만 가입 가능하게 구현

--- service/ToDoList/signup/forms.py
+++ service/ToDoList/signup/forms.py
@@ -3,9 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, required=True, help_text='이메일을 입력하세요.')
+    email = forms.EmailField(max_length=254, help_text='이메일을 입력하세요.')
+    role = forms.ChoiceField(choices=(('user', '일반사용자'), ('admin', '관리자')), required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-        
+        fields = ('username', 'email', 'role', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError('이메일을 입력해주세요.')
+        return email
